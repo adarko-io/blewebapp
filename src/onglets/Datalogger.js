9f3f7ec320178cf4f50536fdd78aa2662a4a4140
Karthi <karthi@clodpi.io>
2024-09-10 13:33:23 +0530

    Fix

--- src/onglets/Datalogger.js
+++ src/onglets/Datalogger.js
@@ -26,7 +26,7 @@ const Datalogger = (props) => {
 
   const [logEntries, setLogEntries] = useState([]);
 
-  
+
   const [currentPage, setCurrentPage] = useState(1);
   const entriesPerPage = 24;
   // Derived state for pagination
@@ -43,7 +43,7 @@ const Datalogger = (props) => {
     pageNumbers.push(i);
   }
 
- 
+
 
   const handleDataloggerTypeChange = (event) => {
     setDataloggerType(event.target.value);
@@ -76,6 +76,23 @@ const Datalogger = (props) => {
     setIsActivated(!isActivated);
   };
 
+  const limitDecimals = value => {
+    const regex = /^\d*\.?\d{0,3}$/;
+    return regex.test(value) ? value : parseFloat(value).toFixed(3);
+  };
+
+  const handleForwardFlowChange = e => {
+    const newForwardFlow = limitDecimals(e.target.value);
+    setForwardFlow(newForwardFlow);
+    setAccumulatedFlow(Number(newForwardFlow) - Number(reverseFlow));
+  };
+
+  const handleReverseFlowChange = e => {
+    const newReverseFlow = limitDecimals(e.target.value);
+    setReverseFlow(newReverseFlow);
+    setAccumulatedFlow(Number(forwardFlow) - Number(newReverseFlow));
+  };
+
   async function handleFormSubmit(event) {
     event.preventDefault(); // Prevent the default form submission
 
@@ -87,12 +104,20 @@ const Datalogger = (props) => {
       const url = document.getElementById('urlInput').value;
       const port = document.getElementById('portInput').value;
       const dataloggerType = document.getElementById('dataloggerTypeSelect').value;
-
       const antenna = document.getElementById('antennaSelect').value;
       const ntpserver = document.getElementById('ntpInput').value;
+      const prescaler = 1;
+      const fwdflow = 0;
+      const revflow = 0;
+
+      if (dataloggerType === 1) {
+        prescaler = document.getElementById('prescalerSelect').value;
+        fwdflow = forwardFlow;
+        revflow = reverseFlow;
+      }
 
       // Combine the form values into a single string or binary format
-      const combinedValue = `${activationStatus},${apn},${interval},${protocol},${url},${port},${dataloggerType},${antenna},${ntpserver}`;
+      const combinedValue = `${activationStatus},${apn},${interval},${protocol},${url},${port},${dataloggerType},${antenna},${ntpserver},${prescaler},${fwdflow},${revflow}`;
 
       // Convert the combined string to a Uint8Array
       let encoder = new TextEncoder();
@@ -181,54 +206,54 @@ const Datalogger = (props) => {
     const voltage2 = (parseInt(hexString.substring(80, 82), 16) + 200) / 100;
 
     return {
-        type: type === 0 ? 'NDC' : 'PDC',
-        timestamp: new Date(timestamp * 1000).toISOString(),
-        timeSyncStatus: timeSyncStatus === 0 ? 'Not Synced' : 'Synced',
-        accFlow,
-        fwdFlow,
-        revFlow,
-        flowRate,
-        systemInfo,
-        errorCode,
-        voltage1: voltage1.toFixed(2),
-        voltage2: voltage2.toFixed(2)
+      type: type === 0 ? 'NDC' : 'PDC',
+      timestamp: new Date(timestamp * 1000).toISOString(),
+      timeSyncStatus: timeSyncStatus === 0 ? 'Not Synced' : 'Synced',
+      accFlow: accFlow.toFixed(3),
+      fwdFlow: fwdFlow.toFixed(3),
+      revFlow: revFlow.toFixed(3),
+      flowRate: flowRate.toFixed(3),
+      systemInfo,
+      errorCode,
+      voltage1: voltage1.toFixed(2),
+      voltage2: voltage2.toFixed(2)
     };
-}
-
-const addLogEntry = (hexString) => {
-  const entry = parseAndPrintHex(hexString);
-  if (entry.timeSyncStatus === "Synced") {
-    setLogEntries(prevEntries => [...prevEntries, entry]);
   }
-};
 
-const WriteDataLoggerSize = async (z) => {
-  try{
-  // Create an ArrayBuffer for Uint16
-  const buffer = new ArrayBuffer(2); // Uint16 takes 2 bytes
-  const dataView = new DataView(buffer);
+  const addLogEntry = (hexString) => {
+    const entry = parseAndPrintHex(hexString);
+    if (entry.timeSyncStatus === "Synced") {
+      setLogEntries(prevEntries => [...prevEntries, entry]);
+    }
+  };
 
-  // Set the value of z at the first position (index 0) in the buffer as Uint16
-  dataView.setUint16(0, z, true); // 'true' for little-endian, 'false' for big-endian
+  const WriteDataLoggerSize = async (z) => {
+    try {
+      // Create an ArrayBuffer for Uint16
+      const buffer = new ArrayBuffer(2); // Uint16 takes 2 bytes
+      const dataView = new DataView(buffer);
 
-  // Write the buffer to the characteristic
-  await ReadWriteCharacteristicLoggerData.characteristic.writeValue(buffer);
-  console.log('Write successful');
-   } catch (error) {
-    console.log('Error:', error);
+      // Set the value of z at the first position (index 0) in the buffer as Uint16
+      dataView.setUint16(0, z, true); // 'true' for little-endian, 'false' for big-endian
+
+      // Write the buffer to the characteristic
+      await ReadWriteCharacteristicLoggerData.characteristic.writeValue(buffer);
+      console.log('Write successful');
+    } catch (error) {
+      console.log('Error:', error);
+    }
   }
-}
 
   const ReadDataLoggerSize = async () => {
     try {
       let endByteReceived = false;
-     // let i = 0;
+      // let i = 0;
       let z = 0;
 
       while (!endByteReceived) {
         // Create an ArrayBuffer and a DataView to handle the Uint16 data
         // Simulate delay (if necessary)
-       
+
         // WriteDataLoggerSize(z++);
         // console.log("loop",z);
         // delay(1000);
@@ -259,7 +284,7 @@ const WriteDataLoggerSize = async (z) => {
           addLogEntry(hexString);
         }
 
-         //i++; // Increment to handle the next Uint16 data index
+        //i++; // Increment to handle the next Uint16 data index
       }
 
     } catch (error) {
@@ -302,6 +327,7 @@ const WriteDataLoggerSize = async (z) => {
 
   const ReadInfo = async () => {
     try {
+      await ReadCharacteristic.characteristic.readValue();
       const value = await ReadCharacteristic.characteristic.readValue();
       const decoder = new TextDecoder('utf-8');
       const result = decoder.decode(value);
@@ -323,34 +349,11 @@ const WriteDataLoggerSize = async (z) => {
     }
   };
 
-  function notifHandler(event) {
-    console.log("Notification Received");
-    var buf = new Uint8Array(event.target.value.buffer);
-    console.log(buf);
 
-  }
-
-
-  async function onNotifyButtonClick() {
-    let notifStatus = document.getElementById('notifyButton').innerHTML;
-    if (notifStatus === "Notify OFF") {
-      console.log('Notification ON');
-      notifyCharacteristic.characteristic.startNotifications();
-      notifyCharacteristic.characteristic.oncharacteristicvaluechanged = notifHandler;
-      document.getElementById('notifyButton').innerHTML = "Notify ON"
-      createLogElement(notifyCharacteristic, 3, "Datalogger ENABLE NOTIFICATION");
-      console.log(notifyCharacteristic.characteristic);
-    } else {
-      notifyCharacteristic.characteristic.stopNotifications();
-      console.log('Notification OFF');
-      document.getElementById('notifyButton').innerHTML = "Notify OFF"
-      createLogElement(notifyCharacteristic, 3, "Datalogger DISABLE NOTIFICATION");
-      console.log(notifyCharacteristic.characteristic);
-    }
-  }
   async function onReadButtonClick() {
     try {
       // Read the value from the characteristic
+      await ReadWriteCharacteristic.characteristic.readValue();
       var value = await ReadWriteCharacteristic.characteristic.readValue();
       let decoder = new TextDecoder('utf-8');
       let result = decoder.decode(value);
@@ -396,8 +399,13 @@ const WriteDataLoggerSize = async (z) => {
       document.getElementById('ntpInput').value = ntpserver;
 
       document.getElementById('prescalerSelect').value = pdcprescaler;
-      document.getElementById('forwardFlowInput').value = fwdflow;
-      document.getElementById('reverseFlowInput').value = revflow;
+      // document.getElementById('forwardFlowInput').value = fwdflow;
+      // document.getElementById('reverseFlowInput').value = revflow;
+
+      setForwardFlow(Number(fwdflow))
+      setReverseFlow(Number(revflow))
+
+      setAccumulatedFlow(Number(fwdflow) - Number(revflow));
 
     } catch (error) {
       console.log('Error: ' + error);
@@ -418,16 +426,18 @@ const WriteDataLoggerSize = async (z) => {
     link.click();
     document.body.removeChild(link);
   };
+
+
   useEffect(() => {
     //console.log(props);
-    onReadButtonClick();
-   // ReadDataLoggerSize();
+    //onReadButtonClick();
+    // ReadDataLoggerSize();
   }, []);
 
   return (
     <div className="container-fluid">
       <div className="container">
-      <div className='row justify-content-center mt-4'>
+        <div className='row justify-content-center mt-4'>
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 p-3 border mx-2 my-2">
             <form>
               <fieldset disabled>
@@ -465,8 +475,8 @@ const WriteDataLoggerSize = async (z) => {
               </fieldset>
               <div className="row">
                 <div className="col-sm-8 offset-sm-4 text-start">
-                <button type="button" className="btn btn-primary me-2" onClick={ReadInfo}>Read</button>
-                  
+                  <button type="button" className="btn btn-primary me-2" onClick={ReadInfo}>Read</button>
+
                 </div>
               </div>
             </form>
@@ -571,23 +581,49 @@ const WriteDataLoggerSize = async (z) => {
               {dataloggerType === '1' && (
                 <>
                   <div className="mb-3 row align-items-center">
-                    <label htmlFor="accumulatedFlowInput" className="col-sm-4 col-form-label text-start">Accumulated Flow</label>
+                    <label htmlFor="accumulatedFlowInput" className="col-sm-4 col-form-label text-start">Accumulated Flow (m<sup>3</sup>)</label>
                     <div className="col-sm-8">
-                      <input type="text" className="form-control" id="accumulatedFlowInput" placeholder="Enter Accumulated Flow" required disabled value={accumulatedFlow} onChange={e => setAccumulatedFlow(e.target.value)} />
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="accumulatedFlowInput"
+                        placeholder="Enter Accumulated Flow"
+                        required
+                        disabled
+                        value={accumulatedFlow}
+                      />
                     </div>
                   </div>
 
                   <div className="mb-3 row align-items-center">
-                    <label htmlFor="forwardFlowInput" className="col-sm-4 col-form-label text-start">Forward Flow</label>
+                    <label htmlFor="forwardFlowInput" className="col-sm-4 col-form-label text-start">Forward Flow (m<sup>3</sup>)</label>
                     <div className="col-sm-8">
-                      <input type="text" className="form-control" id="forwardFlowInput" placeholder="Enter Forward Flow" required disabled={!isActivated} value={forwardFlow} onChange={e => setForwardFlow(e.target.value)} />
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="forwardFlowInput"
+                        placeholder="Enter Forward Flow"
+                        required
+                        disabled={!isActivated}
+                        value={forwardFlow}
+                        onChange={handleForwardFlowChange}
+                      />
                     </div>
                   </div>
 
                   <div className="mb-3 row align-items-center">
-                    <label htmlFor="reverseFlowInput" className="col-sm-4 col-form-label text-start">Reverse Flow</label>
+                    <label htmlFor="reverseFlowInput" className="col-sm-4 col-form-label text-start">Reverse Flow (m<sup>3</sup>)</label>
                     <div className="col-sm-8">
-                      <input type="text" className="form-control" id="reverseFlowInput" placeholder="Enter Reverse Flow" required disabled={!isActivated} value={reverseFlow} onChange={e => setReverseFlow(e.target.value)} />
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="reverseFlowInput"
+                        placeholder="Enter Reverse Flow"
+                        required
+                        disabled={!isActivated}
+                        value={reverseFlow}
+                        onChange={handleReverseFlowChange}
+                      />
                     </div>
                   </div>
 
@@ -616,55 +652,55 @@ const WriteDataLoggerSize = async (z) => {
           </div>
         </div>
         <div className='row justify-content-center mt-4 border mx-2 my-2'>
-        <div className="col-12 mb-3">
+          <div className="col-12 mb-3">
             <button type="button" className="btn btn-primary me-2" onClick={ReadDataLoggerSize}>Read Logged Data</button>
             <button type="button" className="btn btn-secondary" onClick={exportToCSV}>Export Data</button>
           </div>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Type</th>
-                  <th>Timestamp</th>
-                  <th>Time Sync Status</th>
-                  <th>Accumulated Flow</th>
-                  <th>Forward Flow</th>
-                  <th>Reverse Flow</th>
-                  <th>Flow Rate</th>
-                  <th>System Info</th>
-                  <th>Error Code</th>
-                  <th>Voltage</th>
-                 
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Timestamp</th>
+                <th>Time Sync Status</th>
+                <th>Accumulated Flow</th>
+                <th>Forward Flow</th>
+                <th>Reverse Flow</th>
+                <th>Flow Rate</th>
+                <th>System Info</th>
+                <th>Error Code</th>
+                <th>Voltage</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              {currentEntries.map((entry, index) => (
+                <tr key={index}>
+                  <td>{entry.type}</td>
+                  <td>{entry.timestamp}</td>
+                  <td>{entry.timeSyncStatus}</td>
+                  <td>{entry.accFlow}</td>
+                  <td>{entry.fwdFlow}</td>
+                  <td>{entry.revFlow}</td>
+                  <td>{entry.flowRate}</td>
+                  <td>{entry.systemInfo}</td>
+                  <td>{entry.errorCode}</td>
+                  <td>{entry.voltage1} V</td>
                 </tr>
-              </thead>
-              <tbody>
-                {currentEntries.map((entry, index) => (
-                  <tr key={index}>
-                    <td>{entry.type}</td>
-                    <td>{entry.timestamp}</td>
-                    <td>{entry.timeSyncStatus}</td>
-                    <td>{entry.accFlow}</td>
-                    <td>{entry.fwdFlow}</td>
-                    <td>{entry.revFlow}</td>
-                    <td>{entry.flowRate}</td>
-                    <td>{entry.systemInfo}</td>
-                    <td>{entry.errorCode}</td>
-                    <td>{entry.voltage1} V</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <nav>
-              <ul className='pagination'>
-                {pageNumbers.map(number => (
-                  <li key={number} className='page-item'>
-                    <a onClick={() => paginate(number)}  className='page-link'>
-                      {number}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+              ))}
+            </tbody>
+          </table>
+          <nav>
+            <ul className='pagination'>
+              {pageNumbers.map(number => (
+                <li key={number} className='page-item'>
+                  <a onClick={() => paginate(number)} className='page-link'>
+                    {number}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
 
       </div>
     </div>
